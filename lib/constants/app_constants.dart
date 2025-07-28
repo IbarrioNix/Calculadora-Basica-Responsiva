59f5b4b64a3bfce7a5745cfae50a3a40701d9f19
Mario Ibarra <marioibarra1003@gmail.com>
2025-07-28 16:20:50 -0600

    Creacion de constantes

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+
+  static const String appTitle = 'Mi Calculadora';
+
+  static const Color backgroundColor = Colors.black;
+  static const Color displayBackground = Colors.blueGrey;
+  static const Color numberButtonColor = Colors.grey;
+  static const Color operatorButtonColor = Colors.orange;
+  static const Color functionButtonColor = Colors.white24;
+  static const Color textColor = Colors.white;
+
+  static const double mobileButtonSize = 70.0;
+  static const double webButtonSize = 80.0;
+  static const double mobileFontSize = 20.0 ;
+  static const double webFontSize = 24.0;
+  static const double mobileDisplayFontSize = 36.0;
+  static const double webDisplayFontSize = 48.0;
+
+  static const double webBreakpoint = 600.0;
+
+  // 🎯 Theme - COMPLETA ESTE THEME
+  static ThemeData get appTheme => ThemeData(
+    primarySwatch: Colors.blue,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    scaffoldBackgroundColor: Colors.blueGrey,
+  );
+}
